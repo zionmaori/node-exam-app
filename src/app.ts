34b287d63f8760b011c532c867d19a1468b62565
zionmaori <zion.maori@gmail.com>
2021-05-28 12:54:52 +0300

    added unit testing whith humor

--- src/app.ts
+++ src/app.ts
@@ -12,9 +12,15 @@ app.get('/', function(req: any,  res:any) {
   var name = req.query.name
   if(name){
     res.send("Hello " + " "  + name);
+    console.log("p.s. you are awesome" + name )
+  }
+  if(name === ""){
+    res.send("Hello Mister Ghost" )
+    console.log("Whats your name buddy?!")
   }
   else{
     res.send("Hello World");
+    console.log("sometimes the worlds need human - no man query was passed")
   }
   
 });
